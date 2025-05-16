ba84553eed1157e8413c35f6aa3f69d1cde59ccf
Hal <hal.long@outlook.com>
2025-05-16 22:46:13 +0800

    Disable zstd programs, shared libs and tests to avoid CMake version compatibility issues
    Signed-off-by: Hal <hal.long@outlook.com>

--- scripts/build.bat
+++ scripts/build.bat
@@ -3,7 +3,7 @@
 
 rem @call cmake .. -G "Visual Studio 15 2017 Win64" -DEACOPY_BUILD_TESTS:BOOL=ON
 rem @call cmake .. -G "Visual Studio 16 2019" -A x64 -DEACOPY_BUILD_TESTS:BOOL=ON
-@call cmake .. -G "Visual Studio 17 2022" -DEACOPY_BUILD_TESTS:BOOL=ON -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+@call cmake .. -G "Visual Studio 17 2022" -DEACOPY_BUILD_TESTS:BOOL=ON -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DZSTD_BUILD_PROGRAMS=OFF -DZSTD_BUILD_SHARED=OFF -DZSTD_BUILD_TESTS=OFF
 @call cmake --build . --config Release
 @call cmake --build . --config Debug
 
