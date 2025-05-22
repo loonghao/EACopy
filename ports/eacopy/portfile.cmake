2f93a65e80f31be22dc2859fd28c25dbc2721ee6
loonghao <hal.long@outlook.com>
2025-05-23 00:23:51 +0800

    Add portfile.cmake and update .gitignore
    - Add vcpkg portfile.cmake for EACopy package build configuration
    - Update .gitignore to allow .cmake files in ports/ directory
    - This completes the vcpkg registry structure
    
    Signed-off-by: loonghao <hal.long@outlook.com>

---
+++ ports/eacopy/portfile.cmake
@@ -0,0 +1,31 @@
+# EACopy vcpkg portfile
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL https://github.com/loonghao/EACopy.git
+    REF v${VERSION}
+    HEAD_REF master
+)
+
+# Configure CMake
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DEACOPY_BUILD_TESTS=OFF
+        -DEACOPY_BUILD_AS_LIBRARY=ON
+        -DEACOPY_INSTALL=ON
+)
+
+# Build the project
+vcpkg_cmake_build()
+
+# Install the project
+vcpkg_cmake_install()
+
+# Remove debug includes (they are the same as release)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+# Handle copyright
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+# Configure usage
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
